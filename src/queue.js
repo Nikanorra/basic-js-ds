6e24e104e7b2ee07e8ccb53bd3fb69c88ebe5e20
Nikanora <olga.nikanorova@gmail.com>
2025-03-02 22:16:00 +0000

    feat: queue task

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,21 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+class Queue {
+  constructor() {
+    this.start = null;
+    this.end = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.start;
+  }
+
+  enqueue(value) {
+    const newNode = new ListNode(value);
+    if (!this.end) {
+      this.start = this.end = newNode;
+    } else {
+      this.end.next = newNode;
+      this.end = newNode;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.start) {
+      return null;
+    }
+
+    const newValue = this.start.value;
+    this.start = this.start.next;
+    if (!this.start) {
+      this.end = null;
+    }
+
+    return newValue;
+
   }
 }
 
